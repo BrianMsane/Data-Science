b550286e4ff3ca35c19a30e34c4bd007da4cc1ff
Brian Msane <msanebrianboss@gmail.com>
2024-12-29 19:55:05 +0200

    new structure to work with going forward

---
+++ codes/deep-learning-concepts/neuron.py
@@ -0,0 +1,34 @@
+from typing import Union
+
+
+def neuron(inputs: list, weights: list, bias: Union[int, float]):
+    output = 0
+    for input, weight in zip(inputs, weights):
+        output += input * weight
+    return output + bias
+
+
+def layer_neuron(inputs: list, weights: list, biases: Union[int, float]):
+    layer_output = []
+    for n_weight, n_bias in zip(weights, biases):
+        output = neuron(inputs=inputs, weights=n_weight, bias=n_bias)
+        layer_output.append(output)
+    return layer_output
+
+
+if __name__ == "__main__":
+    # inputs = [9, 2, 7]
+    # weights = [.1, .9, -.5]
+    # output = neuron(inputs=inputs, weights=weights, bias=.7)
+    # print("The output of this layer is {}".format(output))
+
+    weights = [
+        [0.2, 0.8, -0.5, 1],
+        [0.5, -0.91, 0.26, -0.5],
+        [-0.26, -0.27, 0.17, 0.87],
+    ]
+
+    inputs = [1, 2, 3, 2.5]
+    biases = [2, 3, 0.5]
+    layer_output = layer_neuron(inputs=inputs, weights=weights, biases=biases)
+    print("The output of this layer is {}".format(layer_output))
