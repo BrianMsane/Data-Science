b8f78adcb71d1f050fdca1869159aae25abcf10f
Brian Msane <msanebrianboss@gmail.com>
2024-12-30 21:26:43 +0200

    tensors with pytorch

--- codes/deep-learning-concepts/neuron.py
+++ codes/deep-learning-concepts/neuron.py
@@ -15,7 +15,7 @@ def neuron(inputs: list[float], weights: list[float], bias: float):
 
 
 def layer_neuron(inputs: list, weights: list, biases: list[float]):
-    """Run all the neurons that are in the layer"""
+    """Run all the neurons that are in the layer and get its output"""
     layer_output = []
     for weights, bias in zip(weights, biases, strict=True):
         output = neuron(inputs=inputs, weights=weights, bias=bias)
@@ -23,20 +23,33 @@ def layer_neuron(inputs: list, weights: list, biases: list[float]):
     return layer_output
 
 
-def network(initial_inputs: list[float], weights: list[float], biases: list[float]):
-    """assuming a fully connected network, run all the layers and get the final output"""
-    pass
+# def network(inputs: list[float], weights: list[float], biases: list[float]):
+#     """Assuming a fully connected network, run all the layers and get the final output"""
+#     outputs = []
+#     for layer_weights, layer_bias in zip(weights, biases):
+#         layer_output = layer_neuron(inputs, layer_weights, layer_bias)
+#         outputs.append(layer_output)
+#         inputs = layer_output  # inputs of the next layer are the outputs of the current layer
+#     return outputs[-1]
 
 
 def main():
     inputs = [0.1, 0.5]
-    weights = [0.2, 0.4]
-    bias = 0.25
-    print("The ouput of the neuron is: ", neuron(inputs, weights, bias))
+    print(
+        "The ouput of the neuron is: ",
+        neuron(inputs=inputs, weights=[0.2, 0.4], bias=0.25),
+    )
 
-    biases = [0.25, 0.25]
+    biases = [[0.25, 0.25], [0.35, 0.35]]
     weights_list = [[0.1, 0.3], [0.2, 0.4]]
-    print("The output of the layer is", layer_neuron(inputs, weights_list, biases))
+    print("The output of the layer is", layer_neuron(inputs, weights_list, biases[0]))
+
+    # predictions = network(
+    #     inputs=inputs,
+    #     weights=[[0.1, 0.3], [0.2, 0.4], [0.5, 0.6], [0.7, 0.8]],
+    #     biases=biases,
+    # )
+    # print("The predicted values are: ", predictions)
 
     # targets = [0.05, 0.95]
     # network_output = None
