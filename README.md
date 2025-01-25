f2809237ebc1278c449beeea234bf311536f4a28
Brian Msane <msanebrianboss@gmail.com>
2025-01-25 21:37:46 +0200

    fix md

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # zindi-learn
+
 Write notes and codes for all the content learnt from zindi courses.
--- codes/deep-learning-concepts/errors.py
+++ codes/deep-learning-concepts/errors.py
@@ -3,3 +3,13 @@ def total_error(y_true: list[float], y_pred: list[float]):
     for output, prediction in zip(y_true, y_pred):
         total += 0.5 * (output - prediction) ** 2
     return total
+
+
+def mse(y_true, y_pred):
+    x_mean = sum(y_pred) / len(y_pred)
+    y_mean = sum(y_true) / len(y_true)
+    # continue the code
+
+
+# TO DO: implement
+# 1. root mean square error
