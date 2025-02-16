c85cca9051c612527dfe24d19e8b9c0a759c59f3
Brian Msane <msanebrianboss@gmail.com>
2025-02-16 15:49:04 +0200

    activation functions

--- dl/activation_functions.py
+++ dl/activation_functions.py
@@ -18,5 +18,6 @@ def hyperbolic_tangent(x: float | int) -> float:
     return (1 + exp(-2(input))) / (1 - exp(-2(input)))
 
 
-def softmax(x: float | int):
-    pass
+def softmax(logits: list[float | int]):
+    exponetials = [exp(pred - max(logits)) for pred in logits]
+    return [j / sum(exponetials) for j in exponetials]
